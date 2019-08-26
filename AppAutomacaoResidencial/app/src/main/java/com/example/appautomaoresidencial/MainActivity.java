f8bb13c4bb4d589b2c66fc00b1f2992705e05136
WillianDomingues79 <willian.souza79@hotmail.com>
2019-08-26 15:02:20 -0300

    Updates e ligação com Reles

--- AppAutomacaoResidencial/app/src/main/java/com/example/appautomaoresidencial/MainActivity.java
+++ AppAutomacaoResidencial/app/src/main/java/com/example/appautomaoresidencial/MainActivity.java
@@ -1,28 +1,52 @@
 package com.example.appautomaoresidencial;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
-    BluetoothAdapter meuBluetooth = null;
+    BluetoothAdapter meuAdapter = null;
+    BluetoothDevice meuDevice = null;
+    BluetoothSocket meuSocket = null;
+
+    UUID meuUUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
     private static final int SOLICITA_ATIVACAO = 1;
     private static final int SOLICITA_CONEXÃO = 2;
+    private static final int MESSAGE_READ = 3;
+
+    ConnectedThread connectedThread;
+
+    Handler mHandler;
+
+    StringBuilder dadosBluetooth = new StringBuilder();
 
     Button btnConexao, btnLed1;
     boolean conexao = false;
 
     private static String MAC = null;
 
+    @SuppressLint("HandlerLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,12 +55,13 @@ public class MainActivity extends AppCompatActivity {
         btnConexao = (Button)findViewById(R.id.btnConexao);
         btnLed1 = (Button)findViewById(R.id.btnLed1);
 
-        meuBluetooth = BluetoothAdapter.getDefaultAdapter();
+        meuAdapter = BluetoothAdapter.getDefaultAdapter();
 
 
-        if (meuBluetooth == null){
+        if (meuAdapter == null){
             Toast.makeText(getApplicationContext(),"Seu Dispositivo não possui Bluetooth", Toast.LENGTH_LONG).show();
-        } else if (!meuBluetooth.isEnabled()){
+            finish();
+        } else if (!meuAdapter.isEnabled()){
             Intent ativaBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(ativaBluetooth,SOLICITA_ATIVACAO);
         }
@@ -44,16 +69,82 @@ public class MainActivity extends AppCompatActivity {
         btnConexao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 if (conexao){
-                    //conectar
+                    try{
+                        //desconectar
+
+                        meuSocket.close();
+                        conexao = false;
+
+                        btnConexao.setText("Conectar");
+
+
+                        Toast.makeText(getApplicationContext(),"O Bluetooth foi desconectado", Toast.LENGTH_LONG).show();
+                    }catch (IOException erro){
+                        Toast.makeText(getApplicationContext(),"Ocorreu um Erro" + erro, Toast.LENGTH_LONG).show();
+                    }
                 } else {
+                    //conectar
                     Intent abreLista = new Intent(MainActivity.this, ListaDispositivos.class);
                     startActivityForResult(abreLista, SOLICITA_CONEXÃO);
                 }
             }
         });
+
+        btnLed1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (conexao) {
+                    connectedThread.enviar("led1");
+
+                }else{
+                    Toast.makeText(getApplicationContext(),"O Bluetooth não está Conectado!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+            //Verificar se mensagem está completa!
+        mHandler = new Handler(){
+
+            @Override
+            public void handleMessage(Message msg) {
+
+                if (msg.what == MESSAGE_READ){
+
+                    String recebidos = (String) msg.obj;
+
+                    dadosBluetooth.append(recebidos);
+
+                    int fimInformacao = dadosBluetooth.indexOf("}");
+
+                    if (fimInformacao > 0){
+
+                        String dadosCompletos = dadosBluetooth.substring(0, fimInformacao);
+
+                        int tamInformacao = dadosCompletos.length();
+
+                        if (dadosBluetooth.charAt(0) == '{'){
+                            String dadosFinais = dadosBluetooth.substring(1, tamInformacao);
+
+                            Log.d(dadosFinais, "handleMessage: Recebidos");
+
+                            if (dadosFinais.contains("l1on")){
+                                btnLed1.setText("LED 1 Ligado");
+                                Log.d("LED1", "Ligado");
+                            } else if(dadosFinais.contains("l1of")){
+                                btnLed1.setText("LED 1 Desligado");
+                                Log.d("LED1", "Desligado");
+                            }
+                        }
+                        dadosBluetooth.delete(0, dadosBluetooth.length());
+                    }
+                }
+            }
+        };
     }
 
+    //INICIO CONEXÃO DO BLUETOOTH
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         //super.onActivityResult(requestCode, resultCode, data);
@@ -72,11 +163,85 @@ public class MainActivity extends AppCompatActivity {
                 if (resultCode == Activity.RESULT_OK){
                     MAC = data.getExtras().getString(ListaDispositivos.ENDERECO_MAC);
 
-                    Toast.makeText(getApplicationContext(),"MacFinal: " + MAC, Toast.LENGTH_LONG).show();
+                    //Toast.makeText(getApplicationContext(),"MacFinal: " + MAC, Toast.LENGTH_LONG).show();
+
+                    meuDevice = meuAdapter.getRemoteDevice(MAC);
+
+                    try {
+                        meuSocket = meuDevice.createRfcommSocketToServiceRecord(meuUUID);
+                        meuSocket.connect();
+                        conexao = true;
+
+                        connectedThread = new ConnectedThread(meuSocket);
+                        connectedThread.start();
+
+                        btnConexao.setText("Desconectar");
+
+                        Toast.makeText(getApplicationContext(),"Você foi conectado com: " + MAC, Toast.LENGTH_LONG).show();
+
+                    }catch (IOException erro){
+                        conexao = false;
+
+                        Toast.makeText(getApplicationContext(),"Ocorreu um Erro: " + erro, Toast.LENGTH_LONG).show();
+                    }
                 } else {
                     Toast.makeText(getApplicationContext(),"Falha ao Obter o MAC", Toast.LENGTH_LONG).show();
 
                 }
         }
     }
+    //FINAL CONEXÃO DO BLUETOOTH
+
+    private class ConnectedThread extends Thread {
+
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+
+                    String dadosBt = new String(buffer, 0, bytes);
+
+                    // Send the obtained bytes to the UI activity
+                    mHandler.obtainMessage(MESSAGE_READ, bytes, -1, dadosBt).sendToTarget();
+
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        public void enviar(String dadosEnviar) {
+            byte[] msgBuffer = dadosEnviar.getBytes();
+            try {
+                mmOutStream.write(msgBuffer);
+            } catch (IOException e) { }
+        }
+
+
+    }
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Repositório Willian - TESTE
+ Repositório de Aplicativo de controle de equipamentos eletrônicos através de uma Aplicação Android.
+
+A Aplicação consiste em mandar um comando para o Arduino que através dele controle um Socket de Relê onde o impulso de aberto ou fechado é dado.
+O objetivo é colocar controladores de voz e acesso de usuário á um Banco de dados onde o Usuário mestre pode escolher, adicionar membros e configurar acionamentos como para quem o botão Panico pode ser enviado e por quem.
+Futuramente adicionar juntamente com o sistema um "porteiro eletronico" com reconhecimento facial onde se pode avisar que X pessoa esteve na sua casa X momento do dia.
